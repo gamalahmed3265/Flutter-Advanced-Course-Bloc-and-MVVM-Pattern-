d28022dd997d154fa48f83f723f64164e4d63a6a
Gamal Ahmed <gamalahmed3265@gmail.com>
2023-10-26 17:26:47 +0300

    init Weather app

--- lib/cubit/points_counter_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'points_counter_cubit.dart';
-
-abstract class PointsCounterState {}
-
-class PointsCounterStateAIncrement extends PointsCounterState {}
-
-class PointsCounterStateBIncrement extends PointsCounterState {}
--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,15 @@
-import 'package:app/points_counter.dart';
 import 'package:flutter/material.dart';
+
+import 'body.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const PointsCounter();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Weather App")),
+      body: const HomePageBody(),
+    );
   }
 }
